80429f59ff8f795605d5c329d630eaf9f2a39a67
Rae Farine <rfarine1027@gmail.com>
2020-11-03 16:43:05 -0500

    Setup prerelease input

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 
 export async function run() {
   try {
     const tagRegex = /^v\d{2}\.\d+\.\d+$/;
     const tag = core.getInput('tag');
+    const prerelease = core.getInput('prerelease');
 
     if (!tag) {
       return core.setFailed('Input tag is required.');
@@ -14,13 +15,21 @@ export async function run() {
       return core.setFailed('Tag must follow format rules: v##.##.##');
     }
 
-    // If not prerelease yet, create release notes for prerelease
+    // Create release notes for prerelease
     // Create prerelease
+    // Note: https://github.com/actions/toolkit/issues/361
+    if (prerelease === 'true') {
+      core.info(`Tag ${tag}: Creating a prerelease...`);
+    }
 
     // If already a prerelease, move to release state
+
+    return core.setOutput('hi');
   } catch (error) {
     return core.setFailed(error.message);
   }
 }
 
 run();
+
+export default run;
--- index.test.js
+++ index.test.js
@@ -1,6 +1,5 @@
 import * as core from '@actions/core';
-import github from '@actions/github';
-import * as action from '.';
+import { run } from '.';
 
 let inputs = {};
 
@@ -14,25 +13,30 @@ jest.spyOn(core, 'getInput').mockImplementation((name) => {
   return inputs[name];
 });
 
-jest.spyOn(core, 'info').mockImplementation(jest.fn());
+jest.spyOn(core, 'info');
 jest.spyOn(core, 'setFailed').mockImplementation(jest.fn());
-jest.spyOn(core, 'setSecret').mockImplementation(jest.fn());
 
 describe('Release', () => {
   describe('run()', () => {
     beforeEach(() => {
-      inputs = {};
+      inputs = { prerelease: 'true' };
     });
 
     test('should exit if no tag input', async () => {
-      await action.run();
+      await run();
       expect(core.setFailed).toHaveBeenCalledTimes(1);
     });
 
     test("should exit if tag isn't properly formatted", async () => {
       inputs.tag = 'this-wont-work';
-      await action.run();
+      await run();
       expect(core.setFailed).toHaveBeenCalledTimes(1);
+    });
+
+    test('should start prerelease steps', async () => {
+      inputs.tag = 'v20.0.00';
+      await run();
+      expect(core.info).toHaveBeenCalledTimes(1);
     });
   });
 });
