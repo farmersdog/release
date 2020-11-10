5600c20d5b0cab072b79222d17aca03cefb75aec
Rae Farine <rfarine1027@gmail.com>
2020-11-10 11:46:00 -0500

    Guard against lacking chType

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ export function formatCommits(commits, chStoryUrl) {
     } = com;
     const regex = /(?<chType>\(\w*\))?(\s)?(?<prMsg>\w*\W*.+?)(\s+)?(\[)?(ch)?(?<chId>\d+)?(\])?\s\(#(?<prNumber>\d+)\)/;
     const matches = message.match(regex);
-    const chType = formatChType(matches.groups.chType);
+    const chType = formatChType(matches && matches.groups.chType);
     const prMsg = matches && matches.groups.prMsg;
     const prLink =
       (matches &&
